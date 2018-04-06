4e9421736b61b2e924a89907bbfb824ec329673a
vadigru <vg@outlook.ie>
2018-04-06 10:33:49 +0300

    Добавил функции

--- js/setup.js
+++ js/setup.js
@@ -7,20 +7,24 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-
-function getRandomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 var wizards = [];
 
-for (var i = 0; i < 4; i++) {
-  wizards[i] = {
-    name: WIZARD_NAMES[getRandomInRange(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomInRange(0, WIZARD_NAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomInRange(0, COAT_COLORS.length)],
-    eyeColor: EYE_COLORS[getRandomInRange(0, EYE_COLORS.length)]
-  };
-}
+var getRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var generateWizard = function (count) {
+  for (var i = 0; i < count; i++) {
+    wizards[i] = {
+      name: WIZARD_NAMES[getRandom(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandom(0, WIZARD_NAMES.length - 1)],
+      coatColor: COAT_COLORS[getRandom(0, COAT_COLORS.length)],
+      eyeColor: EYE_COLORS[getRandom(0, EYE_COLORS.length)]
+    };
+  }
+  return wizards;
+};
+
+generateWizard(4);
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -36,12 +40,15 @@ var renderWizard = function (wizard) {
 
 var similarListElement = userSetup.querySelector('.setup-similar-list');
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+var buildFragments = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  return fragment;
+};
 
-similarListElement.appendChild(fragment);
+similarListElement.appendChild(buildFragments());
 
 userSetup.querySelector('.setup-similar').classList.remove('hidden');
 
