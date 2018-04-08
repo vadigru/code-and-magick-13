1497e9af6906c2e17e582ef119ca455dfeb108b3
vadigru <vg@outlook.ie>
2018-04-08 16:25:56 +0300

    Сделал исправления

--- js/setup.js
+++ js/setup.js
@@ -2,19 +2,21 @@
 
 var userSetup = document.querySelector('.setup');
 userSetup.classList.remove('hidden');
+userSetup.querySelector('.setup-similar').classList.remove('hidden');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var SIMILAR_WIZARDS_COUNT = 4;
 var wizards = [];
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var generateWizard = function (count) {
-  for (var i = 0; i < count; i++) {
+var generateWizard = function (wizardCount) {
+  for (var i = 0; i < wizardCount; i++) {
     wizards[i] = {
       name: WIZARD_NAMES[getRandom(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandom(0, WIZARD_NAMES.length - 1)],
       coatColor: COAT_COLORS[getRandom(0, COAT_COLORS.length)],
@@ -24,7 +26,7 @@ var generateWizard = function (count) {
   return wizards;
 };
 
-generateWizard(4);
+generateWizard(SIMILAR_WIZARDS_COUNT);
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -38,17 +40,14 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var similarListElement = userSetup.querySelector('.setup-similar-list');
-
-var buildFragments = function () {
+var buildFragments = function (wizard) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
+  for (var i = 0; i < wizard.length; i++) {
     fragment.appendChild(renderWizard(wizards[i]));
   }
   return fragment;
 };
 
-similarListElement.appendChild(buildFragments());
+var similarListElement = userSetup.querySelector('.setup-similar-list');
 
-userSetup.querySelector('.setup-similar').classList.remove('hidden');
-
+similarListElement.appendChild(buildFragments(wizards));
